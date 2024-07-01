1f29b29dfe7315b29a7933b0e2938b7f3e02b59f
Axel Denis <axel.denis@epitech.eu>
2024-07-01 18:22:36 +0200

    feat(fuse): moved the fuse boilerplate to a module to start abstraction

---
+++ src/fuse/start.rs
@@ -0,0 +1,123 @@
+use fuser::{
+    FileAttr, FileType, Filesystem, MountOption, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry,
+    Request,
+};
+use libc::ENOENT;
+use std::ffi::OsStr;
+use std::time::{Duration, UNIX_EPOCH};
+
+// FIX - placeholders
+const TTL: Duration = Duration::from_secs(1);
+
+const MOUNT_DIR_ATTR: FileAttr = FileAttr {
+    ino: 1,
+    size: 0,
+    blocks: 0,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::Directory,
+    perm: 0o755,
+    nlink: 2,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+    blksize: 512,
+};
+
+const TEMPLATE_FILE_CONTENT: &str = "Hello World!\n";
+
+const TEMPLATE_FILE_ATTR: FileAttr = FileAttr {
+    ino: 2,
+    size: 13,
+    blocks: 1,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::RegularFile,
+    perm: 0o644,
+    nlink: 1,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+    blksize: 512,
+};
+// ^ placeholders
+
+pub struct FuseController;
+impl Filesystem for FuseController {
+    //Look up a directory entry by name and get its attributes.
+    fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
+        if parent == 1 && name.to_str() == Some("hello.txt") {
+            reply.entry(&TTL, &TEMPLATE_FILE_ATTR, 0);
+        } else {
+            reply.error(ENOENT);
+        }
+    }
+
+    fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
+        match ino {
+            1 => reply.attr(&TTL, &MOUNT_DIR_ATTR),
+            2 => reply.attr(&TTL, &TEMPLATE_FILE_ATTR),
+            _ => reply.error(ENOENT),
+        }
+    }
+
+    fn read(
+        &mut self,
+        _req: &Request,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        _size: u32,
+        _flags: i32,
+        _lock: Option<u64>,
+        reply: ReplyData,
+    ) {
+        if ino == 2 {
+            reply.data(&TEMPLATE_FILE_CONTENT.as_bytes()[offset as usize..]);
+        } else {
+            reply.error(ENOENT);
+        }
+    }
+
+    fn readdir(
+        &mut self,
+        _req: &Request,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        mut reply: ReplyDirectory,
+    ) {
+        if ino != 1 {
+            reply.error(ENOENT);
+            return;
+        }
+
+        let entries = vec![
+            (1, FileType::Directory, "."),
+            (1, FileType::Directory, ".."),
+            (2, FileType::RegularFile, "hello.txt"),
+        ];
+
+        for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
+            // i + 1 means the index of the next entry
+            if reply.add(entry.0, (i + 1) as i64, entry.1, entry.2) {
+                break;
+            }
+        }
+        reply.ok();
+    }
+}
+
+pub fn mount_fuse(mountpoint: &String) {
+    let mut options = vec![MountOption::RO, MountOption::FSName("wormhole".to_string())];
+    options.push(MountOption::AllowOther);
+    options.push(MountOption::AutoUnmount);
+
+    fuser::mount2(FuseController, mountpoint, &options).unwrap();
+}
