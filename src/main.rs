99dabfa8f027c5e2fa8222d02f90a49683fc8b4b
syborg64 <julian.scott@epitech.eu>
2024-07-01 16:34:55 +0200

    feat(cli): add clap cli base

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,52 @@
 // In code we trust
 // AgarthaSoftware - 2024
 
+use clap::Parser;
+
+#[derive(Parser)] // requires `derive` feature
+#[command(name = "wormhole")]
+#[command(bin_name = "wormhole")]
+enum CargoCli {
+    /// make a pod and join a network
+    Join(JoinArgs),
+    /// make a pod and create a new network
+    Create(CreateArgs),
+    /// remove a pod from its network
+    Remove(PodArgs),
+}
+
+#[derive(clap::Args)]
+#[command(version, about, long_about = None)]
+struct PodArgs {
+    #[arg(long, short)]
+    path: Option<std::path::PathBuf>,
+}
+
+
+#[derive(clap::Args)]
+#[command(version, about, long_about = None)]
+struct JoinArgs {
+    #[arg()]
+    url: String,
+    #[arg(long, short)]
+    path: Option<std::path::PathBuf>,
+}
+
+#[derive(clap::Args)]
+#[command(version, about, long_about = None)]
+struct CreateArgs {
+    #[arg()]
+    name: Option<String>,
+    #[arg(long, short)]
+    path: Option<std::path::PathBuf>,
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    match CargoCli::parse() {
+        CargoCli::Join(args) => println!("joining {}", args.url),
+        CargoCli::Create(args) => println!("creating network {:?}", args.name),
+        CargoCli::Remove(_) => println!("removing pod"),
+    }
 }
