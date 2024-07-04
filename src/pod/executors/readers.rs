a8fe3ed7a4b5d96a20496ccdd6d6abe71792e147
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2024-07-04 11:14:49 +0200

    feat(mirror): create function for lookup and getattr

--- src/pod/executors/readers.rs
+++ src/pod/executors/readers.rs
@@ -1,10 +1,29 @@
-use fuser::FileType;
-use std::fs;
+use fuser::{FileAttr, FileType, ReplyAttr, Request};
+use std::fs::{self, Metadata};
+use std::time::{Duration, UNIX_EPOCH};
 use std::{collections::HashMap, io::Error, os::unix::fs::MetadataExt, path::Path};
 use walkdir::WalkDir;
 
 use crate::pod::pod::Pod;
 use crate::pod::COPIED_ROOT;
+
+const TEMPLATE_FILE_ATTR: FileAttr = FileAttr {
+    ino: 2,
+    size: 13,
+    blocks: 1,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::RegularFile,
+    perm: 0o644,
+    nlink: 1,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+    blksize: 512,
+};
 
 impl Pod {
     // NOTE - dev only
@@ -61,6 +80,49 @@ impl Pod {
                     .filter_map(|e| self.file_small_meta(e))
                     .collect(),
             )
+        } else {
+            None
+        }
+    }
+
+    fn modify_metadata_template(data: Metadata, ino: u64) -> FileAttr {
+        let mut attr = TEMPLATE_FILE_ATTR;
+        attr.ino = ino;
+        attr.kind = if data.is_dir() {
+            fuser::FileType::Directory
+        } else if data.is_file() {
+            fuser::FileType::RegularFile
+        } else {
+            fuser::FileType::CharDevice // random to detect unsupported
+        };
+        attr
+    }
+
+    pub fn get_metadata(&self, _req: &Request, ino: u64) -> Option<FileAttr> {
+        if let Some(path) = self.mirror_path_from_inode(ino) {
+            match fs::metadata(path) {
+                Ok(data) => Some(Self::modify_metadata_template(data, ino)),
+                Err(_) => None,
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn lookup_metadata(
+        &self,
+        _req: &Request,
+        parent_ino: u64,
+        file_name: String,
+    ) -> Option<FileAttr> {
+        if let Some(datas) = self.fs_readdir(parent_ino) {
+            let mut metadata: Option<FileAttr> = None;
+            for data in datas {
+                if data.2 == file_name {
+                    metadata = self.get_metadata(_req, data.0);
+                };
+            }
+            metadata
         } else {
             None
         }
