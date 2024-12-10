2d4d0093b78b54182c35f26c3bed4cb50d448414
Axel Denis <axel.denis@epitech.eu>
2024-12-10 12:23:14 +0900

    fix: corrected the network interface with the new merge

--- src/pods/network_interface.rs
+++ src/pods/network_interface.rs
@@ -7,8 +7,8 @@ use fuser::FileType;
 use tokio::{sync::mpsc::UnboundedSender, task::JoinHandle};
 
 use crate::{
-    network::message::{self, NetworkMessage},
-    providers::FsIndex,
+    network::message::{self, ToNetworkMessage},
+    providers::{FsEntry, FsIndex},
 };
 
 use super::disk_manager::DiskManager;
@@ -17,7 +17,7 @@ pub struct NetworkInterface {
     pub arbo: Arc<Mutex<FsIndex>>,
     pub mount_point: PathBuf, // TODO - replace by Ludo's unipath
     pub disk: Arc<DiskManager>,
-    pub network_sender: UnboundedSender<NetworkMessage>,
+    pub network_sender: UnboundedSender<ToNetworkMessage>,
     pub next_inode: Arc<Mutex<u64>>, // TODO - replace with Ino type
     pub network_airport_handle: JoinHandle<()>,
 }
@@ -31,20 +31,19 @@ impl NetworkInterface {
     }
 
     /// Get a new inode, add the requested entry to the arbo and inform the network
-    pub fn register_new_file(&self, ftype: FileType, path: PathBuf) -> u64 {
+    pub fn register_new_file(&self, entry: FsEntry) -> u64 {
         let ino = self.get_next_inode();
 
         {
             let mut arbo = self.arbo.lock().expect("arbo lock error");
-            arbo.insert(ino, (ftype, path.clone()));
+            arbo.insert(ino, entry.clone());
         }
 
         self.network_sender
-            .send(NetworkMessage::File(message::File {
-                path,
-                file: [].to_vec(), // REVIEW - why this field ? useful ?
+            .send(ToNetworkMessage::BroadcastMessage(message::MessageContent::File(message::File {
+                path: entry.get_path().to_path_buf(),
                 ino: ino,
-            }))
+            })))
             .expect("mkfile: unable to update modification on the network thread");
         // TODO - if unable to update for some reason, should be passed to the background worker
 
@@ -52,21 +51,15 @@ impl NetworkInterface {
     }
 
     /// remove the requested entry to the arbo and inform the network
-    pub fn unregister_file(&self, path: PathBuf) -> u64 {
+    pub fn unregister_file(&self, path: PathBuf) {
         {
             let mut arbo = self.arbo.lock().expect("arbo lock error");
-            arbo.retain(|_, (_, pth) | *pth != path)
+            arbo.retain(|_, entry | *entry.get_path() != path)
         }
 
         self.network_sender
-            .send(NetworkMessage::Remove(message::File {
-                path,
-                file: [].to_vec(), // REVIEW - why this field ? useful ?
-                ino: ino,
-            }))
+            .send(ToNetworkMessage::BroadcastMessage(message::MessageContent::Remove(0)))
             .expect("mkfile: unable to update modification on the network thread");
         // TODO - if unable to update for some reason, should be passed to the background worker
-
-        ino
     }
 }
