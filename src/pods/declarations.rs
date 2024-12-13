da1eb3caf4719dc3345b868a200cad63eb63e371
Axel Denis <axel.denis@epitech.eu>
2024-12-13 15:04:13 +0900

    feat: removed old imports & added WhPath for declarations.rs

--- src/pods/declarations.rs
+++ src/pods/declarations.rs
@@ -1,19 +1,13 @@
-use std::{path::PathBuf, sync::Arc};
-
-use openat::Dir;
-use tokio::{sync::mpsc::UnboundedSender, task::JoinHandle};
-
-use crate::{network::{message::NetworkMessage, peer_ipc::PeerIPC}, providers::FsIndex};
+use crate::{network::peer_ipc::PeerIPC, providers::whpath::WhPath};
 
 use super::network_interface::NetworkInterface;
-
 
 // TODO
 pub type PodConfig = u64;
 
 pub struct Pod {
     network_interface: NetworkInterface,
-    mount_point: PathBuf, // TODO - replace by Ludo's unipath
+    mount_point: WhPath, // TODO - replace by Ludo's unipath
     peers: Vec<PeerIPC>,
     pod_conf: PodConfig,
 }
