ba0bbf75d770bf99331cc699e6a81c33d5cd9a18
Axel Denis <axel.denis@epitech.eu>
2024-12-13 15:00:59 +0900

    fix: updated arbo.rs test file with new changes

--- tests/arbo_tests.rs
+++ tests/arbo_tests.rs
@@ -1,30 +1,24 @@
 extern crate wormhole;
-use wormhole::pods::inode::{Arbo, FsEntry, Inode, ROOT};
+use wormhole::pods::arbo::{Arbo, FsEntry, Inode, ROOT};
 
 #[test]
 fn test_inserting_and_retreiving_files() {
     let mut arbo = Arbo::new();
 
     assert!(
-        arbo.add_inode("file1".to_owned(), 1, ROOT, FsEntry::File(Vec::new()),)
+        arbo.add_inode_from_parameters("file1".to_owned(), 1, ROOT, FsEntry::File(Vec::new()),)
             .is_ok(),
         "can't add file1 in / folder"
     );
     assert!(
-        arbo.add_inode(
-            "file2".to_owned(),
-            2,
-            ROOT,
-            FsEntry::File(Vec::new()),
-        )
-        .is_ok(),
+        arbo.add_inode_from_parameters("file2".to_owned(), 2, ROOT, FsEntry::File(Vec::new()),)
+            .is_ok(),
         "can't add file2 in / folder"
     );
 
     assert_eq!(
         Ok((Inode {
             parent: 0,
-            id: 1,
             name: "file1".to_owned(),
             entry: FsEntry::File(Vec::new())
         })),
@@ -34,7 +28,6 @@ fn test_inserting_and_retreiving_files() {
     assert_eq!(
         Ok((Inode {
             parent: 0,
-            id: 2,
             name: "file1".to_owned(),
             entry: FsEntry::File(Vec::new())
         })),
