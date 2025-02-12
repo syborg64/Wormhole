6287fa2f3328d36cff9379cac1b3e2b8459408bf
syborg64 <julian.scott@epitech.eu>
2025-02-12 20:41:00 +0900

    fix(pod): gate os features behind cfg blocks

--- src/pods/declarations.rs
+++ src/pods/declarations.rs
@@ -6,11 +6,15 @@ use tokio::sync::mpsc;
 use tokio::task::JoinHandle;
 #[cfg(target_os = "linux")]
 use fuser;
+#[cfg(target_os = "windows")]
+use winfsp::host::FileSystemHost;
 
-use crate::{
-    fuse::fuse_impl::mount_fuse,
-    network::{message::Address, peer_ipc::PeerIPC, server::Server},
-};
+#[cfg(target_os = "linux")]
+use crate::fuse::fuse_impl::mount_fuse;
+#[cfg(target_os = "windows")]
+use crate::winfsp::winfsp_impl::mount_fsp;
+
+use crate::network::{message::Address, peer_ipc::PeerIPC, server::Server};
 
 use super::{
     arbo::{index_folder, Arbo},
@@ -31,6 +35,8 @@ pub struct Pod {
     pod_conf: PodConfig,
     #[cfg(target_os = "linux")]
     fuse_handle: fuser::BackgroundSession,
+    #[cfg(target_os = "windows")]
+    fsp_host: FileSystemHost<'static>,
     network_airport_handle: Option<JoinHandle<()>>,
     peer_broadcast_handle: Option<JoinHandle<()>>,
     new_peer_handle: Option<JoinHandle<()>>,
@@ -110,6 +116,8 @@ impl Pod {
             pod_conf: config,
             #[cfg(target_os = "linux")]
             fuse_handle: mount_fuse(&mount_point, fs_interface.clone())?,
+            #[cfg(target_os = "windows")]
+            fsp_host: mount_fsp(&mount_point, fs_interface.clone())?,
             network_airport_handle,
             peer_broadcast_handle,
             new_peer_handle,
