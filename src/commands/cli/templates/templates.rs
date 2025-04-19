3141d87561994e878de704d6e0aa1f6a64219fc4
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2025-04-19 20:09:49 +0900

    style: remove one comment

--- src/commands/cli/templates/templates.rs
+++ src/commands/cli/templates/templates.rs
@@ -10,7 +10,6 @@ use crate::pods::whpath::WhPath;
 
 #[must_use]
 pub fn templates(path: &WhPath, name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    //REVIEW - Mettre les template en dehors de la fonction pour plus de lisibilité ? dans un autre fichier ?
     let global_config = default_global_config();
     let local_config = default_local_config(name);
     path.clone().set_absolute();
