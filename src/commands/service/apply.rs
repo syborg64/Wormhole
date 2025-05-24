f383496326903ceda2daf1cc78551d8a7d6027d7
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2025-05-24 13:03:14 +0900

    draft: service apply command

--- src/commands/service/apply.rs
+++ src/commands/service/apply.rs
@@ -1,11 +1,11 @@
-use std::sync::Arc;
+use std::{io, sync::Arc};
 
 use parking_lot::RwLock;
 
 use crate::{
     commands::cli_commands::PodConf,
-    config::{GlobalConfig, LocalConfig},
-    error::{CliError, CliResult},
+    config::{types::Config, GlobalConfig, LocalConfig},
+    error::{CliError}, pods::arbo::{GLOBAL_CONFIG_FNAME, LOCAL_CONFIG_FNAME},
 };
 
 pub async fn apply(
@@ -13,6 +13,22 @@ pub async fn apply(
     global_config: Arc<RwLock<GlobalConfig>>,
     args: PodConf,
 ) -> Result<(), CliError> {
-    
+    for file in args.files.clone() {
+        match file.as_str() {
+            LOCAL_CONFIG_FNAME => {
+                let conf = LocalConfig::read(&args.path.join(LOCAL_CONFIG_FNAME).inner).map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+                let mut local_conf = LocalConfig::write_lock(&local_config, "apply::local_config")?;
+                *local_conf = conf;
+            },
+            GLOBAL_CONFIG_FNAME => {
+                let conf = GlobalConfig::read(&args.path.join(GLOBAL_CONFIG_FNAME).inner).map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+                let mut global_conf = GlobalConfig::write_lock(&global_config, "apply::global_conf")?;
+                *global_conf = conf;
+            },
+            _ => { return Err(CliError::InvalidArgument { arg: file })}
+        }
+    }
+
+
     Ok(())
 }
