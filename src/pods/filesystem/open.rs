3939381c08c25a98ef3e2a2318974e8d68c37b0a
Arthur-Aillet <arthur.aillet@epitech.eu>
2025-05-29 13:21:28 +0900

    feat: create has a custom implementation of open for efficiency, correctness and expandability; open has been factorized to support this change

--- src/pods/filesystem/open.rs
+++ src/pods/filesystem/open.rs
@@ -1,6 +1,6 @@
 use crate::pods::{
     arbo::{Arbo, InodeId},
-    filesystem::file_handle::{AccessMode, FileHandle, FileHandleManager},
+    filesystem::file_handle::{AccessMode, FileHandleManager},
 };
 
 use crate::error::WhError;
@@ -22,6 +22,37 @@ const EXECUTE_BIT_FLAG: u16 = 1u16;
 const WRITE_BIT_FLAG: u16 = 2u16;
 const READ_BIT_FLAG: u16 = 4u16;
 
+pub fn check_permissions(flags: i32, inode_perm: u16) -> Result<AccessMode, OpenError> {
+    match flags & libc::O_ACCMODE {
+        libc::O_RDONLY => {
+            if inode_perm & READ_BIT_FLAG == 0 {
+                Err(OpenError::WrongPermissions)
+            //Behavior is undefined, but most filesystems return EACCES
+            } else if flags & libc::O_TRUNC != 0 {
+                //EACCESS
+                Err(OpenError::TruncReadOnly)
+            //Open is from internal exec syscall
+            } else if flags & FMODE_EXEC != 0 {
+                if inode_perm & EXECUTE_BIT_FLAG == 0 {
+                    Err(OpenError::WrongPermissions)
+                } else {
+                    Ok(AccessMode::Execute)
+                }
+            } else {
+                Ok(AccessMode::Read)
+            }
+        }
+        libc::O_WRONLY if (inode_perm & WRITE_BIT_FLAG == 0) => Err(OpenError::WrongPermissions),
+        libc::O_WRONLY => Ok(AccessMode::Write),
+        libc::O_RDWR if (inode_perm & WRITE_BIT_FLAG == 0 || inode_perm & READ_BIT_FLAG == 0) => {
+            Err(OpenError::WrongPermissions)
+        }
+        libc::O_RDWR => Ok(AccessMode::ReadWrite),
+        //Exactly one access mode flag must be specified
+        _ => Err(OpenError::MultipleAccessFlags),
+    }
+}
+
 impl FsInterface {
     pub fn open(&self, ino: InodeId, flags: i32) -> Result<u64, OpenError> {
         let inode_perm = Arbo::n_read_lock(&self.arbo, "open")?
@@ -29,61 +60,17 @@ impl FsInterface {
             .meta
             .perm;
 
-        let perm = match flags & libc::O_ACCMODE {
-            libc::O_RDONLY => {
-                if inode_perm & READ_BIT_FLAG == 0 {
-                    return Err(OpenError::WrongPermissions);
-                }
-                //Behavior is undefined, but most filesystems return EACCES
-                if flags & libc::O_TRUNC != 0 {
-                    //EACCESS
-                    return Err(OpenError::TruncReadOnly);
-                }
-                //Open is from internal exec syscall
-                if flags & FMODE_EXEC != 0 {
-                    if inode_perm & EXECUTE_BIT_FLAG == 0 {
-                        return Err(OpenError::WrongPermissions);
-                    }
-                    AccessMode::Execute
-                } else {
-                    AccessMode::Read
-                }
-            }
-            libc::O_WRONLY if (inode_perm & WRITE_BIT_FLAG == 0) => {
-                return Err(OpenError::WrongPermissions);
-            }
-            libc::O_WRONLY => AccessMode::Write,
-            libc::O_RDWR
-                if (inode_perm & WRITE_BIT_FLAG == 0 || inode_perm & READ_BIT_FLAG == 0) =>
-            {
-                return Err(OpenError::WrongPermissions);
-            }
-            libc::O_RDWR => AccessMode::ReadWrite,
-            //Exactly one access mode flag must be specified
-            _ => return Err(OpenError::MultipleAccessFlags),
-        };
+        let perm = check_permissions(flags, inode_perm)?;
 
         if flags & libc::O_TRUNC != 0 {
             //TODO: Trunc over the network
         }
 
-        // Nothing to do, the kernel already call make_inode
-        //if flags & libc::O_CREAT != 0 { }
-
-        let direct = flags & libc::O_DIRECT != 0;
-        let no_atime = flags & libc::O_NOATIME != 0;
+        // libc::O_CREAT is never set, The flag is set only with the create syscall
 
         let mut file_handles = FileHandleManager::write_lock(&self.file_handles, "open")?;
-        let uuid = file_handles.new_uuid();
-        file_handles.handles.insert(
-            uuid,
-            FileHandle {
-                uuid,
-                perm,
-                direct,
-                no_atime,
-            },
-        );
-        Ok(uuid)
+        file_handles
+            .insert_new_file_handle(flags, perm)
+            .map_err(|err| err.into())
     }
 }
