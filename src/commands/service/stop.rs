2c786539fc72e9ff1b134473349a03e2e103a11f
syborg64 <julian.scott@epitech.eu>
2025-06-03 18:59:44 +0900

    fix(service): avoid using blocking threads

--- src/commands/service/stop.rs
+++ src/commands/service/stop.rs
@@ -4,12 +4,8 @@ use crate::{
 };
 
 pub async fn stop(pod: Pod) -> Result<CliSuccess, CliError> {
-    let res = tokio::task::spawn_blocking(move || pod.stop())
-        .await
-        .expect("pod stop: can't spawn blocking task")
-        .map(|()| "Pod was stopped.".to_string());
-    match res {
-        Ok(success) => Ok(CliSuccess::Message(success)),
+    match pod.stop() {
+        Ok(()) => Ok(CliSuccess::Message( "Pod was stopped.".to_string())),
         Err(e) => Err(CliError::PodStopError { source: e })
     }
 }
