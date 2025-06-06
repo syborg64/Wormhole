38864f7a4d1c9bc0c55bcf0adbb8a1cd842f557c
syborg64 <julian.scott@epitech.eu>
2025-06-06 17:05:57 +0900

    chore: format

--- src/commands/service/stop.rs
+++ src/commands/service/stop.rs
@@ -5,7 +5,7 @@ use crate::{
 
 pub async fn stop(pod: Pod) -> Result<CliSuccess, CliError> {
     match pod.stop() {
-        Ok(()) => Ok(CliSuccess::Message( "Pod was stopped.".to_string())),
-        Err(e) => Err(CliError::PodStopError { source: e })
+        Ok(()) => Ok(CliSuccess::Message("Pod was stopped.".to_string())),
+        Err(e) => Err(CliError::PodStopError { source: e }),
     }
 }
