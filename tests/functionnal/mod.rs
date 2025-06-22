aca0e30b49429e9f2bd8e2a16f240c3c8c22d23f
Axel Denis <axel.denis@epitech.eu>
2025-06-22 10:32:21 +0900

    feat(tests): corrected log indent

--- tests/functionnal/mod.rs
+++ tests/functionnal/mod.rs
@@ -10,6 +10,7 @@ fn start_log() {
     let _ = env_logger::builder()
         .format_timestamp(None)
         .format_target(false)
+        .format_indent(Some(8))
         .is_test(true)
         .try_init();
 }
