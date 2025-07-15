110129c5009a5a16e81c0df5003fdf4f74c1d9e0
Arthur-Aillet <Arthur-Aillet@users.noreply.github.com>
2025-07-15 14:43:34 +0000

    deploy: 2847c6acb53eb9b8e6829e8be74f97447f79c708

--- trait.impl/alloc/string/trait.ToString.js
+++ trait.impl/alloc/string/trait.ToString.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["wormhole",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"wormhole/network/ip/struct.IpP.html\" title=\"struct wormhole::network::ip::IpP\">IpP</a>"]]]]);
+    var implementors = Object.fromEntries([["wormhole",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"wormhole/network/ip/struct.IpP.html\" title=\"struct wormhole::network::ip::IpP\">IpP</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
