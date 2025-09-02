43d1ceac88aeca3fb734d37a30575626bb9d8b3e
Arthur-Aillet <Arthur-Aillet@users.noreply.github.com>
2025-09-02 10:19:49 +0000

    deploy: 77e59e5b846f584a6fe1192a7341b61a17c0d6a4

--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["wormhole",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"wormhole/network/peer_ipc/struct.PeerIPC.html\" title=\"struct wormhole::network::peer_ipc::PeerIPC\">PeerIPC</a>"]]]]);
+    var implementors = Object.fromEntries([["wormhole",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"wormhole/network/peer_ipc/struct.PeerIPC.html\" title=\"struct wormhole::network::peer_ipc::PeerIPC\">PeerIPC</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
