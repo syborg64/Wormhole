5d7b15c16f7c623ddb48fa6e0706093ca6584fcf
Axel Denis <axel.denis@epitech.eu>
2025-09-02 16:22:44 +0200

    feat(nix-package): nix package now working in repo

--- flake.nix
+++ flake.nix
@@ -25,17 +25,44 @@
         });
 
       packages = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          # The following sources helps downloading the custom winfsp patch
+          # If winfsp in one day sourced on the official repo, could dismiss
+          # this for a single derivation, like was started on this commit:
+          # d968432c3c9b38ddb35da9a20e79dc0a31cf1e74
+          aggregatedSource = pkgs.stdenv.mkDerivation {
+            pname = "cargo-crates-dl";
+            version = "0.0.0";
+            src = fetchGit {
+              url = "https://github.com/Agartha-Software/Wormhole";
+              rev = "65510c91fc23c72e97b318e2a0d79bc5d01e9f51";
+            };
+            doCheck = false;
+            dontFixup = true;
+            nativeBuildInputs = with pkgs; [ cargo rustc cacert wget ];
+            buildPhase = ''
+              runHook preBuild
+              export CARGO_HOME=$PWD/.cargo
+              cargo fetch --locked
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+              mkdir $out
+              cp -r . $out
+              runHook postInstall
+            '';
+            outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+            outputHash = "sha256-O2l82Ezrgxh1P7PXbXoo0Pon9zwyAzFmspT6tqta0so=";
+          };
         in {
-          # https://github.com/NixOS/nixpkgs/blob/4fc53b59aecbc25c0e173163d60155f8fca14bfd/doc/languages-frameworks/rust.section.md
           default = pkgs.stdenv.mkDerivation {
             pname = "wormhole";
             version = "0.1.0";
 
-            src = fetchGit {
-              url = "https://github.com/Agartha-Software/Wormhole";
-              rev = "7c1ad13d3c590c2717e0a75e6ec366067491f830";
-            };
+            src = aggregatedSource;
 
             buildInputs = [ pkgs.rustc pkgs.cargo pkgs.fuse3 pkgs.pkg-config ];
 
