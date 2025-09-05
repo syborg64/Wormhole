2eb72731db3cfa2e8d3ed7230f77e1dac0c8123f
Axel Denis <axel.denis@epitech.eu>
2025-09-05 10:56:56 +0200

    draft(nix): added systemd service

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forEachSystem = nixpkgs.lib.genAttrs systems;
-    in {
+    in rec {
       devShells = forEachSystem (system:
         let pkgs = nixpkgs.legacyPackages.${system};
         in {
@@ -28,5 +28,27 @@
         let pkgs = nixpkgs.legacyPackages.${system};
         in { wormhole = import ./nix/package.nix { inherit pkgs; }; });
 
+      nixosModules.wormhole = { config, pkgs, ... }:
+        let cfg = config.services.wormhole;
+        in {
+          options.services.wormhole = {
+            enable = pkgs.lib.mkEnableOption "Run the Wormhole daemon";
+            package = pkgs.lib.mkOption {
+              type = pkgs.lib.types.package;
+              default = packages.${pkgs.system}.wormholed;
+            };
+          };
+
+          config = pkgs.lib.mkIf cfg.enable {
+            systemd.services.wormhole = {
+              description = "Wormhole Service Daemon";
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig.ExecStart =
+                "${cfg.package}/bin/wormholed 0.0.0.0:8081";
+              serviceConfig.Restart = "on-failure";
+              environment.SERVICE_ADDRESS = "0.0.0.0:8081";
+            };
+          };
+        };
     };
 }
