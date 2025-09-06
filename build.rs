b03b4c881e6cdb418efe1eb07d3ead5915e8d82b
syborg64 <julian.scott@epitech.eu>
2025-09-06 04:38:19 +0200

    chore: format

--- build.rs
+++ build.rs
@@ -9,7 +9,7 @@ fn main() {
         .args(&["rev-parse", "HEAD"])
         .output()
         .ok()
-        .and_then(|output| output.stdout.try_into().ok())
-        .unwrap_or("none".into());
+        .and_then(|output| String::from_utf8(output.stdout).ok())
+        .unwrap_or("none".to_owned());
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
